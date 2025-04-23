a81dc3160ebbd59df24a804a099e3935152b7d98
Zafiro2004 <146875619+Zafiro2004@users.noreply.github.com>
2025-04-23 13:26:19 +0200

    Update Entrega.java

--- Entrega.java
+++ Entrega.java
@@ -50,7 +50,7 @@ import java.util.stream.IntStream;
  * de texte estigui configurat amb codificació UTF-8.
  */
 class Entrega {
-  static final String[] NOMS = {};
+  static final String[] NOMS = {Alex De Dios Pallicer};
 
   /*
    * Aquí teniu els exercicis del Tema 1 (Lògica).
